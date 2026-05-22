acdc41b5bcab859918cace2e5a3dbb2380453b3b
Stav Raviv <stavfx@gmail.com>
2026-05-22 12:28:46 -0700

    Wire up publishing to Maven Central via Vanniktech plugin
    Adds com.vanniktech.maven.publish 0.33.0 with POM metadata (name,
    description, license, developer, SCM) and signs all publications.
    Targets Central Portal with manual release approval. Bump version to
    0.1.0 (was 0.1.0-SNAPSHOT) for the first published artifact.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.maven.publish)
 }
 
 group = "com.stavfx"
-version = "0.1.0-SNAPSHOT"
+version = "0.1.0"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -26,4 +29,43 @@ dependencies {
 
 tasks.withType<Test>().configureEach {
     useJUnit()
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = false)
+    signAllPublications()
+
+    coordinates(group.toString(), "nav3-hilt-vm", version.toString())
+
+    pom {
+        name.set("nav3-hilt-vm")
+        description.set(
+            "KSP processor that takes the boilerplate out of Hilt + Navigation 3 " +
+                "ViewModels with assisted NavKey injection."
+        )
+        inceptionYear.set("2026")
+        url.set("https://github.com/stavfx/nav3-hilt-vm")
+
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("stavfx")
+                name.set("Stav Raviv")
+                url.set("https://github.com/stavfx/")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/stavfx/nav3-hilt-vm/")
+            connection.set("scm:git:git://github.com/stavfx/nav3-hilt-vm.git")
+            developerConnection.set("scm:git:ssh://git@github.com/stavfx/nav3-hilt-vm.git")
+        }
+    }
 }
